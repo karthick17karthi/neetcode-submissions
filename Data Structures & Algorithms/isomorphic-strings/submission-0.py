7b1c301d6a62cf055497f4b5afa46294ec224659
S Karthick <310623243025@eec.srmrmp.edu.in>
2026-07-25 13:33:47 +0530

    Add: isomorphic-strings - submission-0

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        d1={}
+        d2={}
+        for i in range(len(s)):
+            if s[i] not in d1:
+                d1[s[i]]=t[i]
+            else:
+                if(d1[s[i]]!=t[i]):
+                    return False
+            if t[i] not in d2:
+                d2[t[i]]=s[i]
+            if t[i] in d2:
+                if d2[t[i]]!=s[i]:
+                    return False
+        return True
+
+            
