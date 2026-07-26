b6ac88208b5dc8dcffae8c092ac3ecc18bc3c4ea
S Karthick <310623243025@eec.srmrmp.edu.in>
2026-07-26 14:06:35 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d={}
+        for i in strs:
+            s="".join(sorted(i))
+            if s in d:
+                d[s].append(i)
+
+            else:
+                d[s]=[i]
+        return list(d.values())
+
+        
