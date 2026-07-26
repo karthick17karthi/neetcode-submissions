0892fba36466e53cb3df1bf8e8fcd04d1d513633
S Karthick <310623243025@eec.srmrmp.edu.in>
2026-07-26 16:47:33 +0530

    Add: non-cyclical-number - submission-5

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        l=set()
+        temp=n
+        b=True
+        while True:
+            summ=0
+
+            while(temp>0):
+                last=temp%10
+                sq=last*last
+                summ+=sq
+                temp=temp//10
+            temp=summ
+            
+            if summ in l:
+                b=False
+                return False
+            if summ==1:
+                return True
+            else:
+                l.add(summ)
+        return True
