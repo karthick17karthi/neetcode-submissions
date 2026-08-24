bdc3887e42c000d4f25bb0e2d222ed2c10dfe390
S Karthick <310623243025@eec.srmrmp.edu.in>
2026-08-24 21:48:37 +0530

    Add: valid-palindrome-ii - submission-5

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-5.py
@@ -0,0 +1,11 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        rev=s[::-1]
+        if rev==s:
+            return True
+        else:
+            for i in range(len(s)):
+                ans=s[:i]+s[i+1:]
+                if ans==ans[::-1]:
+                    return True
+        return False
